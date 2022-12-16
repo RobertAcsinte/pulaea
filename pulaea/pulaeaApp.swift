6365643cf293af359864d6f3f424d980f44ae2d5
Robert Acsinte <robert.acsinte@gmail.com>
2022-12-16 14:37:32 +0100

    Initial Commit

---
+++ pulaea/pulaeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulaeaApp.swift
+//  pulaea
+//
+//  Created by Robert Acsinte on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct pulaeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
